a32a391535c4641bc96251adab5bd9c979095093
btjdghks2 <btjdghks2@gmail.com>
2023-01-15 22:37:53 +0900

    react_native_basic

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
-
-import { NavigationContainer } from '@react-navigation/native';
-
+import { StatusBar } from 'expo-status-bar';
 import StackNavigator from './navigation/StackNavigator.jsx';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
 
 
 export default function App() {
   return (
 
-    <NavigationContainer>
-      <StratusBar barStyle='light-content'/>
-      <Provider store={store}>
-        <StackNavigator/>
-      </Provider>
-  </NavigationContainer>
-
+    <View style={StyleSheet.container}>
+      <Text> start</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
