691885c8d5259d9cf306803e301052615e3f0af1
btjdghks2 <btjdghks2@gmail.com>
2023-01-15 22:47:05 +0900

    react_navigator

--- App.js
+++ App.js
@@ -2,10 +2,21 @@ import { StatusBar } from 'expo-status-bar';
 import StackNavigator from './navigation/StackNavigator.jsx';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from 'react-native-stack';
 
 
 
 export default function App() {
+
+  const AppNavigator = createStackNavigator(
+    {
+      Home: HomeScreen,
+      Details: DetailsScreen,
+    },
+    {
+      initialRouteName: 'Home',
+    }
+  );
   return (
 
     <View style={StyleSheet.container}>
