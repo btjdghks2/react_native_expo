e19a807253fd9a2ffc1fed08248dde78702ad6f9
btjdghks2 <btjdghks2@gmail.com>
2023-01-18 21:29:28 +0900

    react_study

--- App.js
+++ App.js
@@ -38,20 +38,7 @@ const PreviewLayout = ({
   <View style={{padding:10, flex:1}}>
     <Text style={styles.label}>{label}</Text>
     <View style={styles.row}>
-      {values.map(value => (
-        <TouchableOpacity
-          key={value}
-          onPress={() => setSelectedValue(value)}
-          style={[styles.button, selectedValue === value && styles.selected]}>
-          <Text
-            style={[
-              styles.buttonLabel,
-              selectedValue === value && styles.selectedLabel,
-            ]}>
-            {value}
-          </Text>
-        </TouchableOpacity>
-      ))}
+
     </View>
     <View style={[styles.container, {[label]: selectedValue}]}>{children}</View>
   </View>
