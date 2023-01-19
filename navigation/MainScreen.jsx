4d1df1c6bdd97a9995fe478f9e5694c4b210745b
btjdghks2 <btjdghks2@gmail.com>
2023-01-20 05:37:51 +0900

    Rn_button

--- navigation/MainScreen.jsx
+++ navigation/MainScreen.jsx
@@ -1,19 +1,45 @@
 import React from 'react';
-import {View, Button} from 'react-native'
+import {StyleSheet,View, Button} from 'react-native'
+
+
 
 export const MainScreen = ({navigation}) => {
   return (
-    <View>
-      <Button title="공격"
+    <View style={{marginTop: 30,height:'100%'}}>
+      <View style={{flex:2}} />
+<View style={{flex:1}} >
+      <Button
       color="#8B0000"
+      title="공격"
+      height= "2rem"
       onPress={() => navigation.navigate("Attack")}
-      />
-      <Button title="수비"
+      /></View>
+
+      <View style={{flex:3}} >
+      <Button 
       color="#DAA520"
+      title="수비"
+
       onPress={() => navigation.navigate("Guard")}
-      />
+
+      /></View>
     </View>
 
 
   )
 }
+
+const styles = StyleSheet.create({
+
+ container: {
+  flexDirection: 'column',
+  justifyContent: 'center',
+  marginVertical: '80%',
+ },
+seoarator: {
+  marginVertical: 8,
+  borderBottomColor: '$737373',
+  borderBottomWidth: StyleSheet.hairlineWidth,
+}
+
+})
