2b7f5c6d05526d88f3a9ad6dd3fa0c28768f9ca4
btjdghks2 <btjdghks2@gmail.com>
2023-01-22 03:28:41 +0900

    react_native_main

--- navigation/MainScreen.jsx
+++ navigation/MainScreen.jsx
@@ -1,45 +1,32 @@
 import React from 'react';
-import {StyleSheet,View, Button} from 'react-native'
+import {StyleSheet,View} from 'react-native'
 
 
 
 export const MainScreen = ({navigation}) => {
   return (
-    <View >
-      <View  />
-<View style={{width:'30', height:'40',margin:10}} >
-      <Button
-      color="#8B0000"
-      title="공격"
-      height= "2rem"
-      onPress={() => navigation.navigate("Attack")}
-      /></View>
+    <View style={styles.container}>
 
-      <View style={{width:'30', height:'60',margin:10}} >
-      <Button 
-      color="#DAA520"
-      title="수비"
-
-      onPress={() => navigation.navigate("Guard")}
-
-      /></View>
     </View>
-
-
+    
   )
 }
 
 const styles = StyleSheet.create({
-
- container: {
-  flexDirection: 'column',
-  justifyContent: 'center',
-  marginVertical: '80%',
- },
-seoarator: {
-  marginVertical: 8,
-  borderBottomColor: '$737373',
-  borderBottomWidth: StyleSheet.hairlineWidth,
-}
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingHorizontal: 10,
+  },
+  a_button: {
+    alignItems: 'center',
+    backgroundColor: '#DDDDDD',
+    padding: 10,
+  },
+  b_button: {
+    alignItems: 'center',
+    backgroundColor: '#8FBC8F',
+    padding: 10,
+  }
 
 })
